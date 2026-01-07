d1849718c0cb816dff94d77beadadb47f25f09d1
bella-gatz <102693135+bella-gatz@users.noreply.github.com>
2026-01-06 17:43:34 -0800

    practiced SQL today

---
+++ SQL/selects/addressNull.sql
@@ -0,0 +1,10 @@
+-- Write a solution to report the first name, last name, city, and state of each person in the Person table. 
+-- If the address of a personId is not present in the Address table, report null instead.
+
+
+SELECT p.firstName, p.lastName, a.city, a.state
+FROM Person p
+LEFT JOIN Address a ON a.personId = p.personId
+
+-- Why this works:
+-- Left join keeps all the values from the Person table and also returns the matches from Address table 
